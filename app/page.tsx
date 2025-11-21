b10f3710de63a952fdd10b2742aa00f6a27bd837
chandra-mulyana <motorcb100@gmail.com>
2025-11-21 15:16:34 +0700

    20251121 Update Sample

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 text-center">
+				Sample Trimas - TBAM
+			</h1>
+		</div>
+	);
+}
